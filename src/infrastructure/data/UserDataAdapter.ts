f5a25635059a67f633fb121462295236cf5f9e01
Carlos Díaz <ernesto.diaz@finaktiva.com>
2022-11-05 18:56:36 -0500

    agregado el controlador para ejecutar el servicio rest

--- src/infrastructure/data/UserDataAdapter.ts
+++ src/infrastructure/data/UserDataAdapter.ts
@@ -1,13 +1,12 @@
-import {MongoRepository} from "../repository/MongoRepository";
-import {UserRepository} from "../../domain/models/user/UserRepository";
+import { MongoRepository } from "../repository/MongoRepository";
+import { UserRepository } from "../../domain/models/user/UserRepository";
 
-export class UserDataAdapter extends MongoRepository implements UserRepository{
+export class UserDataAdapter extends MongoRepository implements UserRepository {
+  constructor(entity: any) {
+    super(entity);
+  }
 
-    constructor(entity: any) {
-        super(entity);
-    }
-
-    register(user: any): Promise<any> {
-        return super.save(user);
-    }
+  register(user: any): Promise<any> {
+    return super.save(user);
+  }
 }
--- src/infrastructure/routes/UserRoute.ts
+++ src/infrastructure/routes/UserRoute.ts
@@ -2,12 +2,15 @@ import { Router } from "express";
 import { UserCrudUseCase } from "../../domain/usecase/user/UserCrudUseCase";
 import { UserData } from "../data/UserData";
 import {UserDataAdapter} from "../data/UserDataAdapter";
+import {UserController} from "../controller/user/UserController";
 
 const UserRoute = Router();
 
 const adapter = new UserDataAdapter(UserData);
 const useCase = new UserCrudUseCase(adapter);
+const controller = new UserController(useCase);
 
-UserRoute.post("/", useCase.registerUser);
+
+UserRoute.post("/", controller.registerUser);
 
 export default UserRoute;
