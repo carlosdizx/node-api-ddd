67a34849001ffbf47303a9d7113bac421fde86d7
Carlos Díaz <ernesto.diaz@finaktiva.com>
2022-11-06 22:49:23 -0500

    Creado controlador de person

---
+++ src/infrastructure/controller/person/PersonController.ts
@@ -0,0 +1,14 @@
+import { PersonUseCase } from "../../../domain/usecase/person/PersonUseCase";
+import { Request, Response } from "express";
+
+export class PersonController {
+  constructor(private readonly useCase: PersonUseCase) {}
+
+  public createPerson = async ({ query, body }: Request, res: Response) => {
+    const { uuid } = query;
+    const data: any = body;
+    data.uuid = uuid;
+    const person = await this.useCase.createPerson(data);
+    res.send(person);
+  };
+}
--- src/infrastructure/controller/user/UserController.ts
+++ src/infrastructure/controller/user/UserController.ts
@@ -2,13 +2,13 @@ import { Request, Response } from "express";
 import { UserUseCase } from "../../../domain/usecase/user/UserUseCase";
 
 export class UserController {
-  constructor(private userUseCase: UserUseCase) {
+  constructor(private readonly useCase: UserUseCase) {
     this.findUserByUuid = this.findUserByUuid.bind(this);
   }
 
   public async findUserByUuid({ query }: Request, res: Response) {
     const { uuid } = query;
-    const user = await this.userUseCase.findUserByUuid(`${uuid}`);
+    const user = await this.useCase.findUserByUuid(`${uuid}`);
     res.send(user);
   }
 }
