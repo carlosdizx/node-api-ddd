8b1d70fe82c357ddc23bbd384ca10fec3edb8942
Carlos Díaz <ernesto.diaz@finaktiva.com>
2022-11-06 20:33:44 -0500

    Creado caso de uso para el repository main de user

--- src/domain/usecase/user/UserUseCase.ts
+++ src/domain/usecase/user/UserUseCase.ts
@@ -1,1 +1,9 @@
-export class UserUseCase {}
+import { UserAuthRepository } from "../../models/user/UserRepository";
+
+export class UserUseCase {
+  constructor(private readonly repository: UserAuthRepository) {}
+
+  public findUserByUuid = async (uuid: string) => {
+    return await this.repository.findByUuid(uuid);
+  };
+}
