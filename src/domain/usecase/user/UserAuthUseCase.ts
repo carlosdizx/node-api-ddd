2fbf4d4276c4fab8c584b7b049f9f71dbc2c5373
Carlos Díaz <ernesto.diaz@finaktiva.com>
2022-11-08 10:53:54 -0500

    Modificado caso de uso para adecuarse con las necesidades del cliente

--- src/domain/usecase/user/UserAuthUseCase.ts
+++ src/domain/usecase/user/UserAuthUseCase.ts
@@ -1,14 +1,28 @@
 import { UserAuthRepository } from "../../models/user/UserAuthRepository";
 import { User } from "../../models/user/User";
 import bcrypt from "bcryptjs";
+import { UserRoleRepository } from "../../models/role/UserRoleRepository";
+import { UserRole } from "../../models/role/UserRole";
+import { RoleRepository } from "../../models/role/RoleRepository";
+import { Role } from "../../models/role/Role";
 
 export class UserAuthUseCase {
-  constructor(private readonly repository: UserAuthRepository) {}
+  constructor(
+    private readonly repository: UserAuthRepository,
+    private readonly twoRepository: RoleRepository,
+    private readonly threeRepository: UserRoleRepository
+  ) {}
 
-  public registerUser = async ({ email, password, description }) => {
+  public registerUser = async ({ email, password, description, roles }) => {
     const salt = await bcrypt.genSalt(10);
     const pass = await bcrypt.hash(password, salt);
     const user = new User(email, pass, description);
+    const userSaved = await this.repository.register(user);
+    for (const roleId of roles) {
+      const role: Role = await this.twoRepository.findById(roleId);
+      const userRole: UserRole = new UserRole(role, userSaved);
+      await this.threeRepository.save(userRole);
+    }
     return await this.repository.register(user);
   };
 }
--- src/infrastructure/routes/user/UserAuthRoute.ts
+++ src/infrastructure/routes/user/UserAuthRoute.ts
@@ -1,13 +1,19 @@
 import { Router } from "express";
 import { UserAuthUseCase } from "../../../domain/usecase/user/UserAuthUseCase";
 import { UserData } from "../../data/user/UserData";
-import {UserDataAuthAdapter} from "../../data/user/UserDataAuthAdapter";
-import {UserAuthController} from "../../controller/user/UserAuthController";
+import { UserDataAuthAdapter } from "../../data/user/UserDataAuthAdapter";
+import { UserAuthController } from "../../controller/user/UserAuthController";
+import { RoleDataAdapter } from "../../data/role/RoleDataAdapter";
+import { RoleData } from "../../data/role/RoleData";
+import { UserRoleDataAdapter } from "../../data/role/UserRoleDataAdapter";
+import { UserRoleData } from "../../data/role/UserRoleData";
 
 const userAuthRoute = Router();
 
 const adapter = new UserDataAuthAdapter(UserData);
-const useCase = new UserAuthUseCase(adapter);
+const adapterRole = new RoleDataAdapter(RoleData);
+const adapterUserRole = new UserRoleDataAdapter(UserRoleData);
+const useCase = new UserAuthUseCase(adapter, adapterRole, adapterUserRole);
 const controller = new UserAuthController(useCase);
 
 userAuthRoute.post("/sing-up/", controller.registerUser);
